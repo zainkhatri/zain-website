bdb73eee463421c7ba22d171608d8282a903cd62
Zain Khatri <zainkhatri2560@gmail.com>
2024-08-25 04:55:18 -0700

    new styles

--- src/Bio.js
+++ src/Bio.js
@@ -14,7 +14,7 @@ function Bio() {
       </h2>
       <div className={`content ${isBioVisible ? 'expanded' : ''}`}>
         <p>
-          Hello! I'm Zain Khatri, a dedicated student at the University of California, San Diego, pursuing a degree in Computer Science and Cognitive Science with a specialization in Machine Learning. My passion lies in developing innovative solutions that blend technology and human cognition, with experience ranging from autonomous rovers to brainwave measurement algorithms.
+          Hello! I'm Zain Khatri, a dedicated student at the University of California, San Diego, pursuing a degree in Computer Science and Cognitive Science with a specialization in Machine Learning. My passion lies in developing innovative solutions that blend technology and human cognition.
         </p>
         <p>
           Aside from my technical work, I love to play basketball, learn new songs on the guitar, journal, ride my skateboard through the city, and meet new people when I have the pleasure.
