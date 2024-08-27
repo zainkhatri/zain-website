a8fd2f2ccaab302ce8478821b7df9e034c1981fb
Zain Khatri <zainkhatri2560@gmail.com>
2024-08-27 02:02:13 -0700

    media put into css files instead of media-responsive

--- src/Bio.js
+++ src/Bio.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import './bio.css';
-import './size-responsive.css';
 
 function Bio() {
   const [isBioVisible, setIsBioVisible] = useState(false);
