37fc2b142fed4545d4186414892edc205a4bf81f
Zain Khatri <zainkhatri2560@gmail.com>
2024-08-29 16:05:24 -0700

    Clickable spotify links

--- src/Albums.js
+++ src/Albums.js
@@ -14,6 +14,22 @@ import img11 from './images/testing.jpeg';
 import img12 from './images/dfmb.jpeg';
 import './albums.css';
 
+// Define Spotify links for each album
+const spotifyLinks = [
+  'https://open.spotify.com/album/2ANVost0y2y52ema1E9xAZ',
+  'https://open.spotify.com/album/2XGEyGU76kj55OdHWynX0S',
+  'https://open.spotify.com/album/4PWBTB6NYSKQwfo79I3prg',
+  'https://open.spotify.com/album/6QaVfG1pHYl1z15ZxkvVDW',
+  'https://open.spotify.com/album/7dAm8ShwJLFm9SaJ6Yc58O',
+  'https://open.spotify.com/album/7gsWAHLeT0w7es6FofOXk1',
+  'https://open.spotify.com/album/76290XdXVF9rPzGdNRWdCh',
+  'https://open.spotify.com/album/7ycBtnsMtyVbbwTfJwRjSP', 
+  'https://open.spotify.com/album/1KVGLuPtrMrLlyy4Je6df7', 
+  'https://open.spotify.com/album/2v6ANhWhZBUKkg6pJJBs3B', 
+  'https://open.spotify.com/album/3MATDdrpHmQCmuOcozZjDa', 
+  'https://open.spotify.com/album/3MATDdrpHmQCmuOcozZjDa', 
+];
+
 function Albums() {
   const [isAlbumsVisible, setIsAlbumsVisible] = useState(false);
 
@@ -35,9 +51,9 @@ function Albums() {
           >
             <div className="albums-grid">
               {[img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12].map((img, index) => (
-                <div key={index} className="albums-item">
+                <a key={index} href={spotifyLinks[index]} target="_blank" rel="noopener noreferrer" className="albums-item">
                   <img src={img} alt={`Albums ${index + 1}`} />
-                </div>
+                </a>
               ))}
             </div>
           </motion.div>
