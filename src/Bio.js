b40c5e6c5fea7869d026ce4d3501c61d4e0dfcc0
Zain Khatri <zainkhatri2560@gmail.com>
2024-09-14 15:09:37 -0700

    Edits to bio.js

--- src/Bio.js
+++ src/Bio.js
@@ -25,7 +25,7 @@ function Bio() {
                 Hi, I'm Zain! I'm a Senior at the University of California, San Diego, pursuing a degree in CS/COGSCI & ML. I've been fortunate to work on diverse projects, like developing algorithms for exoskeletons, building autonomous rovers, and create real-time detection and data management. 
               </p>
               <p>
-                Aside from technical work, I love to play basketball, learn new songs on the guitar, ride my skateboard through the city, play chess, and meet new people when I have the pleasure.
+                Aside from technical work, I love to play basketball, learn new songs on the guitar, skateboard, play chess, and meet new people when I have the pleasure.
             </p>
           </motion.div>
         )}
