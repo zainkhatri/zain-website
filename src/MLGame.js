dfcd17f97c2f4623b3bf257457e31a8a4a850409
zainkhatri <zainkhatri2560@gmail.com>
2024-09-29 01:15:22 -0700

    Added better response time for ML Game

--- src/MLGame.js
+++ src/MLGame.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
 import './MLGame.css';
 import { database, ref, set, push, onValue } from './firebase';
 
@@ -259,6 +258,8 @@ const MLGame = () => {
   const handleNumberClick = (number) => {
     if (isGameOver) return;
 
+    console.log('Number clicked:', number.value);
+
     if (number.value === current) {
       setNumbers((prevNumbers) =>
         prevNumbers.map((n) =>
@@ -314,11 +315,9 @@ const MLGame = () => {
   };
 
   return (
-    <motion.div layout className="ml-game">
+    <div className="ml-game">
       <div className="ml-game-header">
-        <div
-          className={`ml-game-timer ${timeLeft <= 10 ? 'red-timer' : ''}`}
-        >
+        <div className={`ml-game-timer ${timeLeft <= 10 ? 'red-timer' : ''}`}>
           {timeLeft}s
         </div>
         <button className="ml-game-button" onClick={handleStartOrRestart}>
@@ -330,27 +329,28 @@ const MLGame = () => {
         <div className="ml-game-description">
           <h2 className="ml-game-title">Eagle Eye: 1-50 in 60</h2>
           <p>
-            Welcome to the Eagle Eye, a game where I test your speed and
-            precision. Your goal is to find and click all the numbers from 1 to
-            50 in ascending order within 60 seconds. Compete to make it to the
-            top 6 scorers list! Click the "Start" button when you're ready to
-            begin. Good luck!
+            Welcome to Eagle Eye, a game where we test your speed and precision.
+            Your goal is to find and click all the numbers from 1 to 50 in
+            ascending order within 60 seconds. Compete to make it to the top 6
+            scorers list! Click the "Start" button when you're ready to begin. Good
+            luck!
           </p>
         </div>
       )}
 
       <div className={`ml-game-grid ${gameStarted ? 'visible' : 'hidden'}`}>
         {numbers.map((number) => (
-          <motion.button
+          <button
             key={number.value}
-            onPointerDown={() => handleNumberClick(number)} // Changed from onClick to onPointerDown
+            onTouchStart={() => handleNumberClick(number)}
+            onMouseDown={() => handleNumberClick(number)}
             className={`ml-game-number ${number.status} ${
               nextNumber === number.value ? 'highlight-next' : ''
             }`}
             disabled={isGameOver}
           >
             {number.value}
-          </motion.button>
+          </button>
         ))}
       </div>
 
@@ -380,33 +380,24 @@ const MLGame = () => {
         </button>
       </div>
 
-      <AnimatePresence>
-        {showHighScores && (
-          <motion.div
-            className="ml-game-high-scores arcade-style"
-            initial={{ opacity: 0, y: -20, scale: 0.9 }}
-            animate={{ opacity: 1, y: 0, scale: 1 }}
-            exit={{ opacity: 0, y: -20, scale: 0.9 }}
-            transition={{ duration: 0.8, ease: 'easeInOut' }}
-            layout
-          >
-            <h3>Leaderboard</h3>
-            {highScores.length > 0 ? (
-              <ol>
-                {highScores.slice(0, 6).map((score, index) => (
-                  // Display only the top 6 scores
-                  <li key={index}>
-                    {score.initials} - {score.score} in {score.time}s
-                  </li>
-                ))}
-              </ol>
-            ) : (
-              <p>No high scores yet. Be the first to set a record!</p>
-            )}
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </motion.div>
+      {showHighScores && (
+        <div className="ml-game-high-scores arcade-style">
+          <h3>Leaderboard</h3>
+          {highScores.length > 0 ? (
+            <ol>
+              {highScores.slice(0, 6).map((score, index) => (
+                // Display only the top 6 scores
+                <li key={index}>
+                  {score.initials} - {score.score} in {score.time}s
+                </li>
+              ))}
+            </ol>
+          ) : (
+            <p>No high scores yet. Be the first to set a record!</p>
+          )}
+        </div>
+      )}
+    </div>
   );
 };
 
