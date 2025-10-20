8dbd68ae642aa30971e0cd7a4a911fc1f647532f
zain khatri <zainkhatri2560@gmail.com>
2025-10-20 15:18:50 -0700

    added nba

--- src/Egomaniac.js
+++ src/Egomaniac.js
@@ -176,6 +176,32 @@ const egoImages = [
   { id: 12, title: "Ego 12", image: ego12 }
 ];
 
+const top10NBAPlayers = [
+  { rank: 1, name: "Lebron", description: "I hate it. He's so corny, and is a avengers team chasing loser, but he can do everything on the court possible, can play any position, has the most points ever, etc. He's an all rounder. He has no aura and is so corny, and it pains me to even put him 1. He is the Drake of the NBA.", image: "https://preview.redd.it/can-somebody-add-the-lebron-james-sunshine-filter-to-this-v0-rr2bj2j3y9zc1.jpg?width=1080&crop=smart&auto=webp&s=73e864d96a0e0a7ad592358e7011794c358ae068" },
+  { rank: 2, name: "MJ", description: "Aura god. He had grown men in fear of him, and so in fear that 30 years later they hop on podcasts talking about how scared they were of Black Jesus. Get a grip.", image: "https://people.com/thmb/TzDJt_cDuFa_EShaPF1WzqC8cy0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(216x0:218x2)/michael-jordan-435-3-4fc019926b644905a27a3fc98180cc41.jpg" },
+  { rank: 3, name: "Kobe", description: "RIP. He's my favorite player of all time, hella clutch, loyal, crazy mentality. He made some of the craziest shots I've ever seen live.", image: "https://i.ebayimg.com/images/g/H74AAOSwDzJj0gva/s-l400.jpg" },
+  { rank: 4, name: "Kareem", description: "He's lwk farming b/c the era was mid. He's Muslim tho so I'll give him his flowers. Bald ass.", image: "https://hips.hearstapps.com/hmg-prod/images/kareem-abdul-jabbar-of-the-los-angeles-lakers-shoots-a-free-news-photo-1643724792.jpg" },
+  { rank: 5, name: "Shaq", description: "The most dominant big boy. Lwk I would take him over Kareem, but Kareem achieved more so whatever. Lwk Shaq is better I'm waffling.", image: "https://preview.redd.it/who-would-you-rather-have-prime-shaq-or-prime-giannis-v0-d0ogt1n4r6pe1.jpg?width=640&crop=smart&auto=webp&s=d72c0c9849e683ce07b4bfbe0ed627556c0247c5" },
+  { rank: 6, name: "Tim Duncan", description: "Off the glass demon. So boring though. His teams were insane, but he was the best player for like 4 of 5 rings. Fair enough.", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNLBENkF0cyxBzStak2g4onlrnuV1kg7_JQg&s" },
+  { rank: 7, name: "Steph", description: "2nd favorite player ever. No explanation needed, he's insane.", image: "https://i.pinimg.com/736x/b7/ba/51/b7ba516ea541e3098afa8c5a6d6a879d.jpg" },
+  { rank: 8, name: "KD", description: "Very similar to Lebron, but just a way better scorer. He's not as good on D, but super fun to watch, super clean offensive demon. He is Steph's chlid, but still he's insane.", image: "https://cdn.prod.website-files.com/5ea7adb57f49ec96cb312e21/61e79cdb5a624971dd78cc1d_hpGJgLb5FAR6qSKuzVCX2BLI3BRB--2SixfFSSQDqlpXqsmTysumeq4HMTeMGBj8GpC2Cdxi_9GUH_0IohJ9D5RwoESWiJx5Heip-_kpNMr_BKBb5fsaD7DDZzq8WlMghzYjk7Hr.jpeg" },
+  { rank: 9, name: "Magic", description: "Controversial at 9 I know. He achieved a lot, but honestly his game isn't that crazy when we look at todays game. He's just Ben Simmons if Ben Simmons was good. Cool passes tho.", image: "https://i.pinimg.com/736x/8e/dd/ba/8eddba2aa9ca03b46e18c062d2a042f3.jpg" },
+  { rank: 10, name: "Hakeem", description: "Shoutout another Muslim brother. 2X rings, super mid team with him. Super fundamental player, footworks insane, super solid center.", image: "https://from-way-downtown.com/wp-content/uploads/2025/01/img_3451.jpeg?w=1568" }
+];
+
+const top10Rappers = [
+  { rank: 1, name: "Kanye", description: "He's gone off the rails thanks to the nitrious, but he's still a generational rapper with an insane catelogue. Super influencial, crazy bars, and he used to be charming.", spotify: "https://open.spotify.com/artist/5K4W6rqBFWDnAN6FQUkS6x", embed: "https://open.spotify.com/embed/artist/5K4W6rqBFWDnAN6FQUkS6x", image: "https://media.gq-magazine.co.uk/photos/5d139cd69fa6013256838bd2/16:9/w_2560%2Cc_limit/kanye-west-01-gq-1june18_getty_b.jpg" },
+  { rank: 2, name: "Kendrick", description: "Let me prefece by saying GNX is mid. HOWEVER, that does not take away from Good Kid Maad City, TPAB, Mr. Morale, and DAMN. Those 4 albums are generational. He's lwk a sell out now, free Drake?", spotify: "https://open.spotify.com/artist/2YZyLoL8N0Wb9xBt1NhZWg", embed: "https://open.spotify.com/embed/artist/2YZyLoL8N0Wb9xBt1NhZWg", image: "https://i.scdn.co/image/ab6761610000e5eb437b9e2a82505b3d93ff1022" },
+  { rank: 3, name: "Travis Scott", description: "Every album he's dropped is so fun. The psychadelic trap beats, the autotune, its fire. Insane concerts live too. Energy is crazy.", spotify: "https://open.spotify.com/artist/0Y5tJX1MQlPlqiwlOH1tJY", embed: "https://open.spotify.com/embed/artist/0Y5tJX1MQlPlqiwlOH1tJY", image: "https://i.scdn.co/image/ab6761610000e5eb19c2790744c792d05570bb71" },
+  { rank: 4, name: "Playboi Carti", description: "My everything. WLR is lwk overrated, but the flows, the beats, etc. He's mumble god.", spotify: "https://open.spotify.com/artist/699OTQXzgjhIYAHMy9RyPD", embed: "https://open.spotify.com/embed/artist/699OTQXzgjhIYAHMy9RyPD", image: "https://cdn-images.dzcdn.net/images/artist/b90097972a60d9d8598a79a786be1a3a/1900x1900-000000-81-0-0.jpg" },
+  { rank: 5, name: "A$AP Rocky", description: "Ever since he married Rihanna he's been a bum, but I love his bars, cloud rap production, and overall vibe. Very solid geezer, just waiting for him to drop...", spotify: "https://open.spotify.com/artist/13ubrt8QOOCPljQ2FL1Kca", embed: "https://open.spotify.com/embed/artist/13ubrt8QOOCPljQ2FL1Kca", image: "https://media.cnn.com/api/v1/images/stellar/prod/190802111010-asap-rocky-file-092018.jpg?q=x_3,y_301,h_1787,w_3177,c_crop/h_833,w_1480" },
+  { rank: 6, name: "Chlidish Gambino", description: "Lowkey underrated in the rap scene. He's a great rapper, funny bars, amazing production, and I miss his old stuff like Because the Internet.", spotify: "https://open.spotify.com/artist/73sIBHcqh3Z3NyqHKZ7FOL", embed: "https://open.spotify.com/embed/artist/73sIBHcqh3Z3NyqHKZ7FOL", image: "https://cdn-p.smehost.net/sites/005297e5d91d4996984e966fac4389ea/wp-content/uploads/2020/01/Childish-Gambino.png" },
+  { rank: 7, name: "Mac Miller", description: "The best white rapper ever. If you disagree you are at least 45 years old.", spotify: "https://open.spotify.com/artist/4LLpKhyESsyAXpc4laK94U", embed: "https://open.spotify.com/embed/artist/4LLpKhyESsyAXpc4laK94U", image: "https://www.rollingstone.com/wp-content/uploads/2018/11/mac-miller-left-behind.jpg?w=1600&h=900&crop=1" },
+  { rank: 8, name: "Don Toliver", description: "He lwk ran 2023. He has a great voice, good chorus, and is solid all around. Hard to hate.", spotify: "https://open.spotify.com/artist/4Ui2kfOqGujY81UcPrb5KE", embed: "https://open.spotify.com/embed/artist/4Ui2kfOqGujY81UcPrb5KE", image: "https://i.scdn.co/image/ab6761610000e5eb52258b97639c24efe49fbf9e" },
+  { rank: 9, name: "JPEGMAFIA", description: "Super versatile. He's lwk a better producer than rapper, but he's amazing at both. Really weird stuff, but I like it a lot.", spotify: "https://open.spotify.com/artist/6yJ6QQ3Y5l0s0tn7b0arrO", embed: "https://open.spotify.com/embed/artist/6yJ6QQ3Y5l0s0tn7b0arrO", image: "https://images.discovery-prod.axs.com/2025/03/jpegmafia-tickets_06-09-25_17_67d859814fc80.jpg" },
+  { rank: 10, name: "Westside Gunn", description: "AYO. He's voice is so fire, sick beats, really interesting lyrics (usually he's waffling), but its fun.", spotify: "https://open.spotify.com/artist/0ABk515kENDyATUdpCKVfW", embed: "https://open.spotify.com/embed/artist/0ABk515kENDyATUdpCKVfW", image: "https://www.billboard.com/wp-content/uploads/2023/10/WESTSIDE-GUNN-press-2023-billboard-1548.jpg?w=1024" }
+];
+
 const DEFAULT_CENTER = [37.5, -122.2];
 const DEFAULT_ZOOM = 8;
 
@@ -225,6 +251,10 @@ const RestaurantMarkers = memo(function RestaurantMarkers({ restaurants, icons, 
 function Egomaniac() {
   const [isFlicksVisible, setIsFlicksVisible] = useState(false);
   const [isMunchVisible, setIsMunchVisible] = useState(false);
+  const [isNBAVisible, setIsNBAVisible] = useState(false);
+  const [isRappersVisible, setIsRappersVisible] = useState(false);
+  const [expandedNBAItems, setExpandedNBAItems] = useState({});
+  const [expandedRapperItems, setExpandedRapperItems] = useState({});
   const [selectedRestaurant, setSelectedRestaurant] = useState(foodRecommendations[0]);
   const [mapInstance, setMapInstance] = useState(null);
   const unsplashAccessKey = resolveUnsplashAccessKey();
@@ -237,8 +267,12 @@ function Egomaniac() {
       : null;
   const flicksContentRef = useRef(null);
   const munchContentRef = useRef(null);
+  const nbaContentRef = useRef(null);
+  const rappersContentRef = useRef(null);
   const [flicksHeight, setFlicksHeight] = useState(0);
   const [munchHeight, setMunchHeight] = useState(0);
+  const [nbaHeight, setNBAHeight] = useState(0);
+  const [rappersHeight, setRappersHeight] = useState(0);
   const [selectedFlickIndex, setSelectedFlickIndex] = useState(null);
   const selectedFlick = selectedFlickIndex !== null ? egoImages[selectedFlickIndex] : null;
 
@@ -372,6 +406,42 @@ function Egomaniac() {
   }, []);
 
   useEffect(() => {
+    if (!nbaContentRef.current) {
+      return undefined;
+    }
+
+    const element = nbaContentRef.current;
+    const updateHeight = () => setNBAHeight(element.scrollHeight);
+    updateHeight();
+
+    if (typeof ResizeObserver === 'undefined') {
+      return undefined;
+    }
+
+    const observer = new ResizeObserver(updateHeight);
+    observer.observe(element);
+    return () => observer.disconnect();
+  }, []);
+
+  useEffect(() => {
+    if (!rappersContentRef.current) {
+      return undefined;
+    }
+
+    const element = rappersContentRef.current;
+    const updateHeight = () => setRappersHeight(element.scrollHeight);
+    updateHeight();
+
+    if (typeof ResizeObserver === 'undefined') {
+      return undefined;
+    }
+
+    const observer = new ResizeObserver(updateHeight);
+    observer.observe(element);
+    return () => observer.disconnect();
+  }, []);
+
+  useEffect(() => {
     if (selectedPhotoIndex === null) {
       return;
     }
@@ -462,6 +532,28 @@ function Egomaniac() {
 
   const toggleMunch = () => {
     setIsMunchVisible((prev) => !prev);
+  };
+
+  const toggleNBA = () => {
+    setIsNBAVisible((prev) => !prev);
+  };
+
+  const toggleRappers = () => {
+    setIsRappersVisible((prev) => !prev);
+  };
+
+  const toggleNBAItem = (rank) => {
+    setExpandedNBAItems((prev) => ({
+      ...prev,
+      [rank]: !prev[rank]
+    }));
+  };
+
+  const toggleRapperItem = (rank) => {
+    setExpandedRapperItems((prev) => ({
+      ...prev,
+      [rank]: !prev[rank]
+    }));
   };
 
   const openFlick = useCallback((index) => {
@@ -710,6 +802,103 @@ function Egomaniac() {
           </div>
         </div>
       </section>
+
+      <section
+        className={`content-section egomaniac-section nba-section ${isNBAVisible ? 'expanded' : ''}`}
+      >
+        <h2 onClick={toggleNBA} className="expandable-title egomaniac-title regular-font">
+          Top 10 NBA Players <span className="toggle-symbol">{isNBAVisible ? '-' : '+'}</span>
+        </h2>
+        <div className="content-wrapper" style={{ '--content-height': `${nbaHeight}px` }}>
+          <div ref={nbaContentRef} className="content egomaniac-content">
+            <div className="top-10-list">
+              {top10NBAPlayers.map((player) => (
+                <div key={player.rank} className="top-10-item">
+                  <div className="rank-badge">{player.rank}</div>
+                  {player.image && (
+                    <div className="rapper-avatar">
+                      <img src={player.image} alt={player.name} className="rapper-avatar-image" />
+                    </div>
+                  )}
+                  <div className="item-content">
+                    <div className="item-header" onClick={() => toggleNBAItem(player.rank)}>
+                      <h3 className="item-name">{player.name}</h3>
+                      <span className="item-toggle">{expandedNBAItems[player.rank] ? '-' : '+'}</span>
+                    </div>
+                    <AnimatePresence initial={false}>
+                      {expandedNBAItems[player.rank] && (
+                        <motion.div
+                          initial={{ opacity: 0, height: 0, marginTop: 0 }}
+                          animate={{ opacity: 1, height: 'auto', marginTop: 8 }}
+                          exit={{ opacity: 0, height: 0, marginTop: 0 }}
+                          transition={{
+                            duration: 0.4,
+                            ease: [0.4, 0.0, 0.2, 1]
+                          }}
+                          style={{ overflow: 'hidden' }}
+                        >
+                          <div className="rapper-profile-expanded">
+                            <p className="item-description">{player.description}</p>
+                          </div>
+                        </motion.div>
+                      )}
+                    </AnimatePresence>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section
+        className={`content-section egomaniac-section rappers-section ${isRappersVisible ? 'expanded' : ''}`}
+      >
+        <h2 onClick={toggleRappers} className="expandable-title egomaniac-title regular-font">
+          Top 10 Rappers <span className="toggle-symbol">{isRappersVisible ? '-' : '+'}</span>
+        </h2>
+        <div className="content-wrapper" style={{ '--content-height': `${rappersHeight}px` }}>
+          <div ref={rappersContentRef} className="content egomaniac-content">
+            <div className="top-10-list">
+              {top10Rappers.map((rapper) => (
+                <div key={rapper.rank} className="top-10-item">
+                  <div className="rank-badge">{rapper.rank}</div>
+                  {rapper.image && (
+                    <div className="rapper-avatar">
+                      <img src={rapper.image} alt={rapper.name} className="rapper-avatar-image" />
+                    </div>
+                  )}
+                  <div className="item-content">
+                    <div className="item-header" onClick={() => toggleRapperItem(rapper.rank)}>
+                      <h3 className="item-name">{rapper.name}</h3>
+                      <span className="item-toggle">{expandedRapperItems[rapper.rank] ? '-' : '+'}</span>
+                    </div>
+                    <AnimatePresence initial={false}>
+                      {expandedRapperItems[rapper.rank] && (
+                        <motion.div
+                          initial={{ opacity: 0, height: 0, marginTop: 0 }}
+                          animate={{ opacity: 1, height: 'auto', marginTop: 8 }}
+                          exit={{ opacity: 0, height: 0, marginTop: 0 }}
+                          transition={{
+                            duration: 0.4,
+                            ease: [0.4, 0.0, 0.2, 1]
+                          }}
+                          style={{ overflow: 'hidden' }}
+                        >
+                          <div className="rapper-profile-expanded">
+                            <p className="item-description">{rapper.description}</p>
+                          </div>
+                        </motion.div>
+                      )}
+                    </AnimatePresence>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+
       <AnimatePresence>
         {selectedPhoto && (
           <motion.div
